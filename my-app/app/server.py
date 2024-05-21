37fb24d2c149edee2fb7d8adba3a3ccf34cc5f1d
i-am-jar <defideraj@gmail.com>
2024-05-21 08:57:24 -0400

    Ready for public

--- my-app/app/server.py
+++ my-app/app/server.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 from fastapi import FastAPI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chat_models import ChatOpenAI
@@ -6,7 +5,7 @@ from langserve import add_routes
 from chain import get_chain
 
 import os
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "ENTER-OPENAI-KEY-HERE"
 
 app = FastAPI(
     title="LangChain Server",
@@ -15,26 +14,11 @@ app = FastAPI(
 )
 
 add_routes(
-    app, 
+    app,
     get_chain(),
     path="/openai",
 )
 
-# add_routes(
-#     app,
-#     ChatAnthropic(),
-#     path="/anthropic",
-# )
-
-# model = ChatAnthropic()
-prompt = ChatPromptTemplate.from_template("tell me a joke about {topic}")
-add_routes(
-    app,
-    prompt,
-    path="/joke",
-)
-
 if __name__ == "__main__":
     import uvicorn
-
     uvicorn.run(app, host="localhost", port=8000)
